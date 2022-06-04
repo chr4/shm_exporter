f39a2fc0ec939bad4a7a4432130ebfa2cd4eed7e
Chris Aumann <me@chr4.org>
2022-06-04 22:04:16 +0200

    Add Prometheus support

--- main.go
+++ main.go
@@ -2,59 +2,106 @@ package main
 
 import (
 	"encoding/binary"
+	"flag"
+	"fmt"
 	"log"
+	"net/http"
 	"net"
+	"os"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	promVersion "github.com/prometheus/common/version"
 )
 
 const (
-	defaultMulticastAddress = "239.12.255.254:9522"
-	maxDatagramSize         = 8192
+	maxDatagramSize = 8192
 )
 
+func init() {
+	promVersion.Version = "0.1.0"
+	prometheus.MustRegister(promVersion.NewCollector("sma_home_manager_exporter"))
+}
+
 func main() {
-	listen(defaultMulticastAddress, msgHandler)
-}
+	var (
+		listenAddr   = flag.String("web.listen-address", ":9192", "The address to listen on for HTTP requests.")
+		multicastAddr = flag.String("multicast.addr", "239.12.255.254:9522", "Multicast address to listen on.")
+		pollInterval = flag.Int("inverter.poll-interval", 5, "Interval in seconds between polls.")
+		showVersion  = flag.Bool("version", false, "Print version information and exit.")
+	)
 
-func msgHandler(src *net.UDPAddr, n int, b []byte) {
+	flag.Parse()
 
-	// TODO: Check this
-	// b[16:18] == '\x60\x69'
-	// b[0:3] == "SMA"
-
-	// TODO: function to convert byte range to float
-	totalPowerIn := b[34:36]
-	totalPowerOut := b[54:56]
-	out := float64(binary.BigEndian.Uint16(totalPowerOut)) * 0.1
-	in := float64(binary.BigEndian.Uint16(totalPowerIn)) * 0.1
-
-	log.Printf("out: %f in: %f", out, in)
-}
-
-// Listen binds to the UDP address and port given and writes packets received
-// from that address to a buffer which is passed to a hander
-func listen(address string, handler func(*net.UDPAddr, int, []byte)) {
-	// Parse the string address
-	addr, err := net.ResolveUDPAddr("udp4", address)
-	if err != nil {
-		log.Fatal(err)
+	if *showVersion {
+		fmt.Printf("%s\n", promVersion.Print("sma_home_manager_exporter"))
+		os.Exit(0)
 	}
 
-	// Open up a connection
-	conn, err := net.ListenMulticastUDP("udp4", nil, addr)
-	if err != nil {
-		log.Fatal(err)
-	}
+	var shmOutWatts = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "shm_out_watts",
+		Help: "SMA Home Manager 2.0 outbound power (sell)",
+	})
+	var shmInWatts = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "shm_in_watts",
+		Help: "SMA Home Manager 2.0 inbound power (buy)",
+	})
 
-	conn.SetReadBuffer(maxDatagramSize)
+	// Register the summary and the histogram with Prometheus's default registry
+	prometheus.MustRegister(shmOutWatts)
+	prometheus.MustRegister(shmInWatts)
 
-	// Loop forever reading from the socket
-	for {
-		buffer := make([]byte, maxDatagramSize)
-		numBytes, src, err := conn.ReadFromUDP(buffer)
+	// Add Go module build info
+	prometheus.MustRegister(collectors.NewBuildInfoCollector())
+
+	// Poll inverter values
+	go func() {
+
+		// Parse the string address
+		addr, err := net.ResolveUDPAddr("udp4", *multicastAddr)
 		if err != nil {
-			log.Fatal("ReadFromUDP failed:", err)
+			log.Fatal(err)
 		}
 
-		handler(src, numBytes, buffer)
-	}
+		// Open up a connection
+		conn, err := net.ListenMulticastUDP("udp4", nil, addr)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		conn.SetReadBuffer(maxDatagramSize)
+
+		// Loop forever reading from the socket
+		for {
+			buffer := make([]byte, maxDatagramSize)
+			_, _, err := conn.ReadFromUDP(buffer)
+			if err != nil {
+				log.Fatal("ReadFromUDP failed:", err)
+			}
+
+			// TODO: Check this
+			// buffer[16:18] == '\x60\x69'
+			// buffer[0:3] == "SMA"
+
+			// TODO: function to convert byte range to float
+			totalPowerIn := buffer[34:36]
+			totalPowerOut := buffer[54:56]
+			out := float64(binary.BigEndian.Uint16(totalPowerOut)) * 0.1
+			in := float64(binary.BigEndian.Uint16(totalPowerIn)) * 0.1
+
+			shmInWatts.Set(in)
+			shmOutWatts.Set(out)
+
+			time.Sleep(time.Duration(*pollInterval) * time.Second)
+		}
+	}()
+
+	// Expose the registered metrics via HTTP
+	http.Handle("/metrics", promhttp.HandlerFor(
+		prometheus.DefaultGatherer,
+		promhttp.HandlerOpts{},
+	))
+	log.Fatal(http.ListenAndServe(*listenAddr, nil))
 }
